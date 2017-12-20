421d8b4910e7a24326a71fdc1f6b0becc82d89e8
Lev Fomenko <shadow.of.lion@inbox.ru>
2017-12-20 23:52:52 +0300

    Добавьте файлы проекта.

---
+++ intecomTest/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intecomTest
+{
+    struct ArhivePoint
+    {
+        public double Data { private set; get; }
+
+        public DateTime DateTimePoint { private set; get; }
+
+        public ArhivePoint(double dataArr, DateTime dt)
+        {
+            Data = dataArr;
+            DateTimePoint = dt;
+        }
+    }
+
+    class TimeArhive {
+
+        bool filled = false;
+        int currentIndex = -1;
+
+        ArhivePoint[] arhiveArr;
+
+        public TimeArhive(int size)
+        {
+            arhiveArr = new ArhivePoint[size];
+        }
+
+        public void EnqueueLast(ArhivePoint elem)
+        {
+            currentIndex++;
+            if (currentIndex > arhiveArr.Length - 1)
+            {
+                currentIndex = 0;
+                filled = true;
+            }
+            arhiveArr[currentIndex] = elem;
+        }
+
+        public int FirstIndex
+        {
+            get
+            {
+                if (filled)
+                {
+                    return (currentIndex <= arhiveArr.Length - 1) ? currentIndex + 1 : 0;
+                }
+                return this.Empty ? -1 : 0;
+            }
+        }
+
+        public bool Empty
+        {
+            get { return currentIndex < 0; }
+        }
+
+        // ближайший индекс в массиве по временной точке
+        public int GetNearestIndexFromTimePoint(DateTime dt)
+        {
+            if (this.Empty){
+                return -1;
+            }
+            if (arhiveArr[currentIndex].DateTimePoint == dt)
+            {
+                return currentIndex;
+            }
+            else if (arhiveArr[currentIndex].DateTimePoint > dt)
+            {
+                if (arhiveArr[FirstIndex].DateTimePoint > dt) // временная точка за границами архива 
+                {
+                    return -1;
+                }
+
+                // приблизительный сдвиг от текущего положения в массиве
+                int index_shift = (arhiveArr[currentIndex].DateTimePoint - dt).Seconds;
+
+                int newIndex;
+                if (index_shift > arhiveArr.Length)
+                {
+                    newIndex = FirstIndex; 
+                }
+                else
+                { 
+                    newIndex = currentIndex - index_shift;
+                    if (newIndex < 0)
+                    {
+                        newIndex = newIndex + arhiveArr.Length - 1;
+                    }
+                }
+                for(int i = 0; i < arhiveArr.Length; i++)
+                {
+                    arhiveArr[newIndex].DateTimePoint
+                }
+            }
+        }
+
+        private int NextIndex(int index)
+        {            
+            if (filled)
+            {
+                return index < currentIndex ? index + 1 :
+                    () ;
+            }
+            if (index >= currentIndex)
+            {
+                return -1;
+            }
+        }
+
+        public double GetAvgData(DateTime dt_start, DateTime dt_end)
+        {
+            // Первый элемент
+            int start_index = 0;
+            
+            arhiveArr[currentIndex].DateTimePoint 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
